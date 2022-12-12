0e4887b5e656f1a5227ed0410fa15070ae24d79a
clobber <brymaster@gmail.com>
2022-12-11 22:37:46 -0700

    Update to mGBA 0.9.3

--- src/gba/renderers/gl.c
+++ src/gba/renderers/gl.c
@@ -933,6 +933,7 @@ void GBAVideoGLRendererReset(struct GBAVideoRenderer* renderer) {
 	glRenderer->nextPalette = 0;
 	glRenderer->paletteDirtyScanlines = GBA_VIDEO_VERTICAL_PIXELS;
 	memset(glRenderer->shadowRegs, 0, sizeof(glRenderer->shadowRegs));
+	glRenderer->shadowRegs[REG_DISPCNT >> 1] = glRenderer->dispcnt;
 	glRenderer->regsDirty = 0xFFFFFFFFFFFEULL;
 
 	int i;
@@ -947,6 +948,9 @@ void GBAVideoGLRendererReset(struct GBAVideoRenderer* renderer) {
 
 void GBAVideoGLRendererWriteVRAM(struct GBAVideoRenderer* renderer, uint32_t address) {
 	struct GBAVideoGLRenderer* glRenderer = (struct GBAVideoGLRenderer*) renderer;
+	if (renderer->cache) {
+		mCacheSetWriteVRAM(renderer->cache, address);
+	}
 	glRenderer->vramDirty |= 1 << (address >> 12);
 }
 
@@ -958,8 +962,9 @@ void GBAVideoGLRendererWriteOAM(struct GBAVideoRenderer* renderer, uint32_t oam)
 
 void GBAVideoGLRendererWritePalette(struct GBAVideoRenderer* renderer, uint32_t address, uint16_t value) {
 	struct GBAVideoGLRenderer* glRenderer = (struct GBAVideoGLRenderer*) renderer;
-	UNUSED(address);
-	UNUSED(value);
+	if (renderer->cache) {
+		mCacheSetWritePalette(renderer->cache, address >> 1, mColorFrom555(value));
+	}
 	glRenderer->paletteDirty = true;
 	int r = M_R5(value);
 	int g = M_G5(value) << 1;
@@ -1432,10 +1437,14 @@ void GBAVideoGLRendererDrawScanline(struct GBAVideoRenderer* renderer, int y) {
 	}
 
 	if (GBARegisterDISPCNTGetMode(glRenderer->dispcnt) != 0) {
-		glRenderer->bg[2].affine.sx += glRenderer->bg[2].affine.dmx;
-		glRenderer->bg[2].affine.sy += glRenderer->bg[2].affine.dmy;
-		glRenderer->bg[3].affine.sx += glRenderer->bg[3].affine.dmx;
-		glRenderer->bg[3].affine.sy += glRenderer->bg[3].affine.dmy;
+		if (glRenderer->bg[2].enabled == 4) {
+			glRenderer->bg[2].affine.sx += glRenderer->bg[2].affine.dmx;
+			glRenderer->bg[2].affine.sy += glRenderer->bg[2].affine.dmy;
+		}
+		if (glRenderer->bg[3].enabled == 4) {
+			glRenderer->bg[3].affine.sx += glRenderer->bg[3].affine.dmx;
+			glRenderer->bg[3].affine.sy += glRenderer->bg[3].affine.dmy;
+		}
 	}
 }
 
